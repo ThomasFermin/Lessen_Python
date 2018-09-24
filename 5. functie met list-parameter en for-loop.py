6e5de5168302a306f55d68aaf0e5df330ac1c325
Thomas <tfer97@hotmail.nl>
2018-09-24 16:54:24 +0200

    Second commit

--- 5. functie met list-parameter en for-loop.py
+++ 5. functie met list-parameter en for-loop.py
@@ -1,10 +1,11 @@
-import math
 def kwadraten_som():
+    total = 0
     grondgetallen=[]
     for i in range(5):
         x=int(input("Please enter a number: "))
-        grondgetallen.append(x)
-    positive_values = list(filter(lambda x: x >0, lst))
-    square_sum=math.sqrt(positive_values)
-    print(square_sum)
+        if x > 0:
+            grondgetallen.append(x)
+    for i in grondgetallen:
+        total = total + i * i
+    print(total)
 kwadraten_som()
